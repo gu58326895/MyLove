226c629e63176c7c8e44965e447f7d70ada3f99e
冯承祥 <feng.chengxiang@darcytech.com>
2017-02-14 15:38:34 +0800

    lodash 相关

--- src/main/web/lib/wrapLodash.js
+++ src/main/web/lib/wrapLodash.js
@@ -1,19 +1,39 @@
 import _set from 'lodash/set'
 import _get from 'lodash/get'
 import _map from 'lodash/map'
-import _chain from 'lodash/chain'
 import _each from 'lodash/each'
 import _compact from 'lodash/compact'
 import _uniq from 'lodash/uniq'
 import _find from 'lodash/find'
 
-export default {
+const obj = {
    set: _set,
    get: _get,
    map: _map,
-   chain: _chain,
    each: _each,
    compact: _compact,
    uniq: _uniq,
    find: _find
 }
+
+const chain = (value) => {
+   let chain = new Object();
+   chain.currentValue = value;
+   _each(obj, (v, k) => {
+      chain[k] = (arg1, arg2) => {
+         let val = v(chain.currentValue, arg1, arg2);
+         chain.currentValue = val || chain.currentValue;
+         return chain;
+      }
+   });
+   chain.value = () => chain.currentValue;
+   return chain;
+}
+
+let wrap = new Object();
+_each(obj, (v, k) => {
+   wrap[k] = obj[k];
+});
+wrap.chain = chain;
+
+export default wrap;
